cba1620da0de56d4e945cbf7bee060d059599121
Nikita Lafinskiy <niklafinskiy@gmail.com>
2021-08-19 14:45:19 +0300

    bug fixes

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,12 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import vision from '@google-cloud/vision';
+// import vision from '@google-cloud/vision';
 
 export default async function handler(req, res) {
-  const client = new vision.ImageAnnotatorClient();
-  const fileName =
-    'C:\\Users\\MSI\\Desktop\\workout-gym-next\\public\\favicon.ico';
-  const [result] = await client.imageProperties(fileName);
-  const colors = result.imagePropertiesAnnotation.dominantColors.colors;
-  colors.forEach((color) => console.log(color));
+  // const client = new vision.ImageAnnotatorClient();
+  // const fileName =
+  //   'C:\\Users\\MSI\\Desktop\\workout-gym-next\\public\\favicon.ico';
+  // const [result] = await client.imageProperties(fileName);
+  // const colors = result.imagePropertiesAnnotation.dominantColors.colors;
+  // colors.forEach((color) => console.log(color));
   res.status(200).json({ name: 'ass' });
 }
--- pages/detect.js
+++ pages/detect.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 function Detect(props) {
-  axios.get('http://localhost:3000/api/hello').then((doc) => {
-    console.log(doc);
-  });
+  // axios.get('http://localhost:3000/api/hello').then((doc) => {
+  //   console.log(doc);
+  // });
 
   return <div>hey</div>;
 }
