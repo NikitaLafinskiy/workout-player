e3b5c720efeb0e67e51ab0ff2d8dc6155c1ad581
Nikita Lafinskiy <niklafinskiy@gmail.com>
2021-10-28 19:23:56 +0300

    modified the form

--- pages/modify.js
+++ pages/modify.js
@@ -1,25 +1,83 @@
-import React, { useEffect, useState, useContext } from "react";
-import Image from "next/image";
-import FormInput from "../Components/FormInput";
-import FormWrapper from "../Components/FormWrapper";
-import Button from "../Components/Button";
-import styles from "../styles/modify.module.css";
+import React, { useEffect, useState, useContext } from 'react';
+import Image from 'next/image';
+import FormInput from '../Components/FormInput';
+import FormWrapper from '../Components/FormWrapper';
+import Button from '../Components/Button';
+import styles from '../styles/modify.module.css';
 
 function Index(props) {
+  const [state, setState] = useState({
+    both: false,
+    full: true,
+  });
+  const [forms, setForms] = useState(null);
+  const handleChangeCheckbox = (e) => {
+    setState((prev) => {
+      return {
+        ...prev,
+        [e.target.name]: e.target.checked,
+      };
+    });
+    console.log(e.target.checked);
+  };
+
+  useEffect(() => {
+    const { both, full } = state;
+    both
+      ? full
+        ? setForms(
+            <>
+              <FormInput placeholder="The first playlist's link: " />
+              <FormInput placeholder="The second playlist's link: " />
+            </>
+          )
+        : setForms(
+            <>
+              <FormInput placeholder="The first playlist's link: " />
+              <FormInput placeholder="The second playlist's link: " />
+              <FormInput placeholder='Time for a song from the first playlist to play for: ' />
+              <FormInput placeholder='Time for a song from the second playlist to play for: ' />
+            </>
+          )
+      : full
+      ? setForms(
+          <>
+            <FormInput placeholder="The playlist's link: " />
+          </>
+        )
+      : setForms(
+          <>
+            <FormInput placeholder="The playlist's link: " />
+            <FormInput placeholder='Time for a song from the playlist to play for: ' />
+          </>
+        );
+  }, [state]);
+
+  const out = forms ? forms : <></>;
   return (
     <>
       <FormWrapper>
         <h1>Options: </h1>
         <div className={styles.checkbox}>
-          <input type='checkbox' /> <p>Two playlists</p>
+          <input
+            name='both'
+            onChange={handleChangeCheckbox}
+            defaultChecked={state.both}
+            type='checkbox'
+          />
+          <p>Two playlists</p>
         </div>
         <div className={styles.checkbox}>
-          <input type='checkbox' /> <p>Play full songs</p>
+          <input
+            name='full'
+            onChange={handleChangeCheckbox}
+            defaultChecked={state.full}
+            type='checkbox'
+            type='checkbox'
+          />
+          <p>Play full songs</p>
         </div>
-        <FormInput placeholder="The first playlist's link: " />
-        <FormInput placeholder="The second playlist's link: " />
-        <FormInput placeholder='Time for a song from the first playlist to play for: ' />
-        <FormInput placeholder='Time for a song from the second playlist to play for: ' />
+        {out}
         <Button> Submit</Button>
       </FormWrapper>
 
